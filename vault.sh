dc1118c6fab98a741320dc8f88b4621d95ef835e
Alexander Shevchenko <kudato@me.com>
2020-01-05 14:58:11 +0300

    Add proxy protocol envs.

--- vault.sh
+++ vault.sh
@@ -8,7 +8,7 @@ for i in \
     AWS_ACCESS_KEY_ID,AWS_S3_ACCESS_KEY_ID="" \
     AWS_SECRET_ACCESS_KEY,AWS_S3_SECRET_ACCESS_KEY="" \
     AWS_REGION,AWS_DEFAULT_REGION,AWS_KMS_REGION=us-east-1 \
-    AWS_KMS_KEY_ID=False \
+    AWS_KMS_KEY_ID=false \
     VAULT_SERVER_MAX_LEASE_TTL=1h \
     VAULT_SERVER_DEF_LEASE_TTL=1h \
     VAULT_SERVER_DISABLE_MLOCK=false \
@@ -17,7 +17,8 @@ for i in \
     VAULT_SERVER_ADDR=0.0.0.0 \
     VAULT_SERVER_PORT=8200 \
     VAULT_SERVER_UI=true \
-    VAULT_SERVER_PROTOCOL_BEHAVIOR=use_always
+    VAULT_SERVER_PP_BEHAVIOR,VAULT_SERVER_PROXY_PROTOCOL_BEHAVIOR=use_always \
+    VAULT_SERVER_PP_AUTH_ADDRS,VAULT_SERVER_PROXY_PROTOCOL_AUTH_ADDRS=false
 do
     defaultEnv "${i}"
 done
@@ -33,15 +34,16 @@ storage "s3" {
 }
 
 listener "tcp" {
- address     = "${VAULT_SERVER_ADDR}:${VAULT_SERVER_PORT}"
- tls_disable = ${VAULT_SERVER_TLS_DISABLE}
- proxy_protocol_behavior = "${VAULT_SERVER_PROTOCOL_BEHAVIOR}"
+  address     = "${VAULT_SERVER_ADDR}:${VAULT_SERVER_PORT}"
+  tls_disable = ${VAULT_SERVER_TLS_DISABLE}
+  proxy_protocol_behavior = "${VAULT_SERVER_PROTOCOL_BEHAVIOR}"
+  ##TCPproxy_protocol_authorized_addrs = "${VAULT_SERVER_PROXY_PROTOCOL_AUTH_ADDRS}"
 }
 
-##seal "awskms" {
-##  region     = "${AWS_KMS_REGION}"
-##  kms_key_id = "${AWS_KMS_KEY_ID}"
-##}
+##KMSseal "awskms" {
+##KMS  region     = "${AWS_KMS_REGION}"
+##KMS  kms_key_id = "${AWS_KMS_KEY_ID}"
+##KMS}
 
 ui = ${VAULT_SERVER_UI}
 max_lease_ttl = "${VAULT_SERVER_MAX_LEASE_TTL}"
@@ -55,7 +57,12 @@ then
     setcap cap_ipc_lock=+ep "$(readlink -f "$(which vault)")"
 fi
 
-if [[ "${AWS_KMS_KEY_ID}" != "False" ]]
+if [[ "${AWS_KMS_KEY_ID}" != "false" ]]
 then
-    sed -i "s|##||g" /config.hcl
+    sed -i "s|##TCP||g" /config.hcl
+fi
+
+if [[ "${VAULT_SERVER_PROXY_PROTOCOL_AUTH_ADDRS}" != "false" ]]
+then
+    sed -i "s|##KMS||g" /config.hcl
 fi
